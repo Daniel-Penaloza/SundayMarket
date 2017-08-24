67717da13f19ed03db2c15e5fb293650374aa5f9
Ollie <oliverbucks@gmail.com>
2017-08-24 20:25:42 +0000

    add categories model, validations, crud, category card, show page for products etc.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
 	has_many :products
 	has_many :users, :through  => :products
+	
+	#add validations, uniqueness, ? think about what else
+	
+  mount_uploader :cat_image, CategoryImageUploader
+    validates_presence_of :name
+
 end
