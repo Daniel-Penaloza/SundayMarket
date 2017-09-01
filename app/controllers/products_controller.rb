09d40320067e415d9dc1f82afbd4ea69621fcb61
Rafael Prallon <final_rpmx@hotmail.com>
2017-09-01 13:20:16 -0300

    Complete product basic crud

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,56 @@
 class ProductsController < ApplicationController
-	def index
-		@products = Product.all
-	end
-
-	def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    @product.user_id = current_user.id
-    @product.description.split(',')
-		if @product.save
-      flash[:success] = "Product has been added to the catalogue"
-      redirect_to products_path
-    else
-      flash[:warning] = "Product has not been added to the catalogue"
-      render 'new'
+  
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_product, only: [:show, :edit, :update, :destroy] 
+  
+    def index
+      @products = Product.all
     end
-	end
-	
-private
-	def set_product
-		@product = Product.friendly.find(params[:id])
-	end
-
-	def product_params
-		params.require(:product).permit(:category_id, :name, :price, :summary, description: [])
-	end
-end
+  
+    def new
+      @product = Product.new
+    end
+  
+    def create
+      @product = Product.new(product_params)
+      @product.user = current_user
+      @product.description.split(',')
+      if @product.save
+        flash[:success] = "Product has been added to the catalogue"
+        redirect_to products_path
+      else
+        flash[:warning] = "Product has not been added to the catalogue"
+        render 'new'
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      @product.description.split(',')
+      if @product.update(product_params)
+        flash[:success] = "Product has been updated"
+        redirect_to products_path
+      else
+        render 'edit'
+      end
+    end
+  
+    def show
+    end
+  
+    def destroy
+      @product.destroy
+      flash[:danger] = "product has been removed from the catalogue"
+      redirect_to products_path
+    end
+  
+    private
+      def set_product
+        @product = Product.find(params[:id])
+      end
+      def product_params
+        params.require(:product).permit(:category_id, :name, :price, :summary, description: [])
+      end
+  end
