cdc6feb1c2481ca112dce9eaa7b7802bb88ed7ee
Daniel Penaloza <danielpenalozatj@gmail.com>
2017-08-31 18:58:29 -0700

    Show view for products completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.paginate(:page => params[:page], :per_page => 6)
 	end
+
+	def show
+		@product = Product.friendly.find(params[:id])
+	end
 end
