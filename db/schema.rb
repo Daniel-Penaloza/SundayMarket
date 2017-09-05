4ab20ffe5260491e0fdd6a5ef1401d6bb328620c
Daniel Penaloza <danielpenalozatj@gmail.com>
2017-09-05 15:33:05 -0700

    Added shop_name attribute to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903221009) do
+ActiveRecord::Schema.define(version: 20170905222707) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20170903221009) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "shop_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug"
