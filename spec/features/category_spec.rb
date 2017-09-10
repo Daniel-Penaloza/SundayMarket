ae8cd70fdf8ac9aa22c125e898ec55a7a1e6b2c0
Daniel Penaloza <danielpenalozatj@gmail.com>
2017-09-09 20:08:20 -0700

    Applying some styles and creation of tests for categories

--- spec/features/category_spec.rb
+++ spec/features/category_spec.rb
@@ -40,6 +40,27 @@ describe 'navigate index' do
 		visit categories_path
 		expect(page).to have_content("2 products in this category.")
 	end
+
+
+	it 'an admin is available to see the edit and delete actions' do
+		@category = FactoryGirl.create(:technology)
+		@admin = FactoryGirl.create(:admin_user)
+		login_as(@admin)
+
+		visit categories_path
+		expect(page).to have_link("edit_#{@category.id}_category")
+		expect(page).to have_link("delete_#{@category.id}_category")
+	end
+
+	it 'an regular user cannot see the edit and delete actions' do
+		@category = FactoryGirl.create(:technology)
+		@user = FactoryGirl.create(:user)
+		login_as(@user)
+
+		visit categories_path
+		expect(page).to_not have_link("edit_#{@category.id}_category")
+		expect(page).to_not	have_link("delete_#{@category.id}_category")
+	end
 end
 
 describe 'navigate show' do
@@ -74,7 +95,13 @@ describe 'navigate show' do
 			expect(description.name).to eq("[\"Excellent Status 10 of 10\", \"The most coolest phone\", \"Liberated for any carrier\"]")
 		end
 	end
+end
 
+describe "navigate edit" do
+	before do
+		@category = FactoryGirl.create(:technology)
+	end
+	
 	it 'cannot edit a category if you are not the admin' do
 		@regular_user = FactoryGirl.create(:other_user)
 		login_as(@regular_user)
@@ -84,19 +111,46 @@ describe 'navigate show' do
 		expect(page).to have_content("You are not authorized to perform this action.")
 	end
 
-	it 'just an admin can edit a category ' do
+	it 'can edit a category id admin' do
 		@admin = FactoryGirl.create(:admin_user)
 		login_as(@admin)
 
-		visit edit_category_path(@category)
-		fill_in "category[name]", 	with: "Cycling"
-		fill_in "category[color]", 	with: "#fff"
-		click_on("Update The Category")
+		visit categories_path
+		expect(page.status_code).to eq(200)
 
+		click_on("edit_#{@category.id}_category")
+		expect(current_path).to eq(edit_category_path(@category))
+
+		fill_in "category[name]", with: "Music"
+		fill_in "category[color]", with: "#f39c12"
+
+		click_on("Update The Category")
 		expect(current_path).to eq(category_path(@category))
-		expect(page).to have_content("Category successfully updated.")
-		expect(page).to have_content("Cycling")
-		expect(page).to have_content("2 products in this category")
+		expect(page).to have_content("Music")
 	end
 end
 
+describe "navigate create" do
+	it 'only an admin can create a category' do
+		@admin = FactoryGirl.create(:admin_user)
+		login_as(@admin)
+		visit new_category_path
+
+		fill_in "category[name]", with: "Office"
+		fill_in "category[color]", with: "#fff"
+		click_on("Create A Category")
+
+		expect(current_path).to eq(categories_path)
+		expect(page).to have_content("The category was created successfully.")
+		expect(page).to have_content("Office")
+	end
+
+	it 'a regular user cannot create a category' do
+		@user = FactoryGirl.create(:user)
+		login_as(@user)
+
+		visit new_category_path
+		expect(current_path).to eq(root_path)
+		expect(page).to have_content("You are not authorized to perform this action.")
+	end
+end
