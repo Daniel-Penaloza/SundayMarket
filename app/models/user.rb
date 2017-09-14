2493b617b7763cb12321cfa88a345913c8fae8fa
Rafael Prallon <final_rpmx@hotmail.com>
2017-09-14 17:13:44 -0300

    fix category_no_repeated method

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
 
   #-----------Avoiding The Repeated Elements In The User Categories------------#
   def category_no_repeated
-      self.categories.select(:id, :name, :color).uniq
+      self.categories.select(:id, :name, :slug, :color).uniq
   end
 
   #-----------------------print if user is banned or not-----------------------#
